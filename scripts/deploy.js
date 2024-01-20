2e52d7c4534c5887725994fa04d0d8005a35590c
Otaiki1 <otaikisadiq2804@gmail.com>
2024-01-20 20:06:27 +0100

    refac: update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy(initBalance);
-  await assessment.deployed();
+  const BuyMeADrink = await hre.ethers.getContractFactory("BuyMeADrink");
+  const buyMeADrink = await BuyMeADrink.deploy();
+  await buyMeADrink.deployed();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+  console.log(`BuyMeADrink contract  deployed to ${buyMeADrink.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
